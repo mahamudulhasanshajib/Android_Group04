5becce76009dfd68a97d5d7a9265e5345125c3ef
Sadab Mostafa <sadab.mostafa@bjitacademy.com>
2022-11-23 15:30:19 +0600

    sadab parsi

--- arraySearch.java
+++ arraySearch.java
@@ -1,4 +1,4 @@
-public class LinearSearchExample{    
+hello!
 public static int linearSearch(int[] arr, int key){    
         for(int i=0;i<arr.length;i++){    
             if(arr[i] == key){    
