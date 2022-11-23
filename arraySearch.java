a05172e1e042599f842e448dfaeadc8b65e22435
Sadab Mostafa <sadab.mostafa@bjitacademy.com>
2022-11-23 15:04:42 +0600

    new change

--- arraySearch.java
+++ arraySearch.java
@@ -8,7 +8,7 @@ public static int linearSearch(int[] arr, int key){
         return -1;    
     }    
     public static void main(String a[]){    
-        int[] a3= {10,20,30,50,70,90,100,150,180};    
+        string a3= "string"    
         int key = 50;    
         System.out.println(key+" is found at index: "+linearSearch(a3, key));    
     }    
